53889283914da01dedd8e707b437056c39b8abc6
Xuan Wang <xuan.wang22@imperial.ac.uk>
2022-11-15 09:50:53 +0000

    LV model

---
+++ Week7/code/LV1.py
@@ -0,0 +1,46 @@
+from scipy import stats
+import numpy as np
+import scipy.integrate as integrate
+import matplotlib.pylab as p
+import matplotlib.pyplot as plt
+
+def dCR_dt(pops, t=0):
+    """
+    This function generates the growth rate of consumer and resource population at any given time
+    """
+    R = pops[0]
+    C = pops[1]
+    dRdt = r * R - a * R * C 
+    dCdt = -z * C + e * a * R * C
+    return np.array([dRdt, dCdt])
+
+r = 1.
+a = 0.1 
+z = 1.5
+e = 0.75
+t = np.linspace(0, 15, 1000)
+
+R0 = 10
+C0 = 5 
+RC0 = np.array([R0, C0])
+pops, infodict = integrate.odeint(dCR_dt, RC0, t, full_output=True)
+
+f1 = p.figure()
+p.subplot(2, 1, 1)
+p.plot(t, pops[:,0], 'g-', label='Resource density') # Plot
+p.plot(t, pops[:,1]  , 'b-', label='Consumer density')
+p.grid()
+p.legend(loc='best')
+p.xlabel('Time')
+p.ylabel('Population density')
+p.title('Consumer-Resource population dynamics')
+
+# generating the graph for resource density and consumer density
+p.subplot(2, 1, 2)
+p.plot(pops[:,0], pops[:,1], 'r-')
+p.grid()
+p.xlabel('Resource density')
+p.ylabel('Consumer density')
+p.title('Consumer-Resource population dynamics')
+
+f1.savefig('../results/LV_model.pdf') #Save figure
