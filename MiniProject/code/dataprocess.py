218ab2d77b0c5573cd2dd54dd9dd16f19d17a23e
Xuan Wang <xuan.wang22@imperial.ac.uk>
2022-11-29 23:53:07 +0000

    miniproject

--- MiniProject/code/dataprocess.py
+++ MiniProject/code/dataprocess.py
@@ -1,69 +1,24 @@
+__author__ = "Xuan Wang (xuan.wang22@imperial.ac.uk)"
+__date__ = "27 Nov 2022"
+
 # import packages
 import pandas as pd
 import scipy as sc
 import numpy as np
-import matplotlib.pylab as plt
-import seaborn as sns 
-from lmfit import Minimizer, Parameters, report_fit
-
 
 # load data
-data = pd.read_csv("../data/LogisticGrowthData.csv")
-data_meta = pd.read_csv("../data/LogisticGrowthMetaData.csv")
-print("Loaded {} columns.".format(len(data.columns.values)))
+data_subset = pd.read_csv("../data/LogisticGrowthData.csv")
+pd.read_csv("../data/LogisticGrowthMetaData.csv")
+
+# ensure the time records are all non-negative
+data_subset["Time"] = abs(data_subset["Time"])
+data_subset["PopBio"] = abs(data_subset["PopBio"])
 
 # insert single growth curve by combining species, medium, temp and citation columns
-data.insert(0, "ID", data.Species + "_" + data.Temp.map(str) + "_" + data.Medium + "_" + data.Citation)
-data_subset = data[data['ID']=='Chryseobacterium.balustinum_5_TSB_Bae, Y.M., Zheng, L., Hyun, J.E., Jung, K.S., Heu, S. and Lee, S.Y., 2014. Growth characteristics and biofilm formation of various spoilage bacteria isolated from fresh produce. Journal of food science, 79(10), pp.M2072-M2080.'] # replace with numbers
+data_subset.insert(0, "ID", data_subset.Species + "_" + data_subset.Temp.map(str) + "_" + data_subset.Medium + "_" + data_subset.Citation)
 
-# plot the graph of the data
-sns.lmplot(x= "Time", y = "PopBio", data = data_subset, fit_reg = False)
-t = np.array(data['Time'])
+# factorisation for subset analysis
+data_subset.fact_ID = pd.factorize(data_subset.ID)
 
-#################################
-# fit linear model to the data
-#################################
-
-######### NLLS fitting ##########
-parm_linear = Parameters()
-parm_linear.add('a', value = 1)
-parm_linear.add('b', value = 1)
-parm_linear.add('c', value = 1)
-parm_linear.add('d', value = 1)
-
-def residuals_linear(params, t, data):
-    """Calculating cubic growth and substract data"""
-    v = params.valuesdict()
-    lin_model = v['a'] * t ** 3 + v['b'] * t ** 2 + v['c'] * t  + v['d']
-    return lin_model - data
-
-mini = Minimizer(residuals_linear, parm_linear, fcn_args = (data_subset["Time"], data_subset["PopBio"]))
-fit_linear_NLLS = mini.minimize()
-
-# summary of the fit
-report_fit(fit_linear_NLLS)
-
-######### OLS fitting ###########
-fit_linear_OLS = np.polyfit(data_subset["Time"], data_subset["PopBio"], 3)
-print(fit_linear_OLS)
-
-######### Comparison between OLS and NLLS fitting ##########
-
-print(fit_linear_NLLS.params)
-
-par_dict = fit_linear_NLLS.params.valuesdict().values()
-par = np.array(list(par_dict))
-print(fit_linear_OLS - par)
-
-poly_eqn = np.poly1d(fit_linear_OLS)
-predicted_values = poly_eqn(data_subset["Time"])
-residuals = predicted_values - data_subset["PopBio"]
-
-##################################
-###### Non-linear model fit ######
-##################################
-
-# calculate the growth rate from the linear fit
-parm_logistic = Parameters()
-parm_logistic.add('N_0', value = data_subset["PopBio"][0])
-parm_logistic.add('N_max', value = list(data_subset["PopBio"])[-1])
+# save the data
+data_subset.to_csv("../data/data_subset.csv")
--- MiniProject/code/miniproject.R
+++ MiniProject/code/miniproject.R
@@ -1,196 +1,345 @@
+###################################################################
+# Prerequisite 
+###################################################################
+
+# clean up
+rm(list = ls())
+
 # import the required packages
-library(ggplot2)
+require("ggplot2")
 require("minpack.lm")
+require("tidyverse")
+require("tidyr")
 
 # load dataset
-data_subset <- read.csv("../results/data_subset.csv")
+data_subset <- read.csv("../data/data_subset.csv")
+
+# data preparation
 t <- as.numeric(data_subset$Time)
-pop <- as.numeric(data_subset$PopBio)
-log_pop <- log(pop)
-data_subset$log_pop <- log_pop
+data_subset$log_pop <- log(data_subset$PopBio)
 
-################################
-##### fitting linear model #####
-################################
+# factorizing the the dataframe for subsets
+data_subset$ID <- as.factor(data_subset$ID)
 
-# quadratic
-quadratic_model <- lm(log_pop ~ poly(Time, 2), data = data_subset)
-quadratic_predict <- predict.lm(quadratic_model, data.frame(Time = t))
+###################################################################
+# Definition of models
+###################################################################
 
-df1 <- data.frame(t, quadratic_predict)
-df1$model <- "Linear_Quadratic"
-names(df1) <- c("Time", "LogN", "model")
-
-# cubic
-cubic_model <- lm(log_pop ~ poly(Time, 3), data = data_subset)
-cubic_predict <- predict.lm(cubic_model, data.frame(Time = t))
-
-df2 <- data.frame(t, cubic_predict)
-df2$model <- "Linear_Cubic"
-names(df2) <- c("Time", "LogN", "model")
-
-###############################
-####### Buchanan model ########
-###############################
-
-# function for Buchanan model
-# lag phase - t<=tlag: logNt = logN0
-# exponential growth phase - tlag < t < tmax: logNt = logN0 + k(t - tlag)
-# stationary phase - t>=tmax: logNt = logNmax + k(tmax - t)
-buchanan_model <- function(t, t_lag, t_max, N_0, k){
-    return(N_0 * I(t <= t_lag) + (N_0 + k * (t - t_lag)) * I(t > t_lag & t < t_max) + (N_0 + k * (t_max - t_lag)) * I(t >= t_max))
+# Buchanan model
+buchanan_model <- function(t, t_lag, t_max, N_max, N_0, r_max){
+    t_max <- t_lag + ((N_max - N_0) / r_max)
+    return(N_0 * I(t <= t_lag) + (N_0 + r_max * (t - t_lag)) * I(t > t_lag & t < t_max)
+     + (N_max) * I(t >= t_max) & (t >= t_lag))
 }
 
-# defining starting parameters
-k <- 0.004
-N_0 <- min(log_pop)
-t_lag <- data_subset$Time[which.max(diff(diff(data_subset$log_pop)))]
-t_max <- data_subset$Time[which.max(diff(diff(data_subset$log_pop[data$Time > t_lag_start])))]
+# Logistic model
+logistic_model <- function(t, r_max, N_max, N_0){
+    return(N_0 * N_max * exp(r_max * t)/(N_max + N_0 * (exp(r_max * t) - 1)))
+}
 
-# fitting the model
-fit_buchanan <- nlsLM(log(PopBio) ~ buchanan_model(t = Time, t_lag, t_max, N_0, k),
-                        data = data_subset,
+# Gompertz model
+gompertz_model <- function(t, r_max, N_max, N_0, t_lag){
+    return(N_0 + (N_max - N_0) * exp(-exp(r_max * exp(1) * (t_lag - t)/((N_max - N_0) * log(10)) + 1)))
+}
+
+# Baranyi model
+baranyi_model <- function(t, N_0, N_max, r_max, t_lag){
+    return(N_0 + r_max * (t + (1/r_max) * log(exp(-r_max * t)
+    + exp(-t_lag) - exp(-r_max * t - t_lag)))
+    -log(1+(exp(r_max*t + log(exp(-r_max*t+exp(-t_lag)-exp(-r_max*t-t_lag))))-1)
+    / exp(N_max - N_0))
+    )
+}
+
+# defining AIC and BIC calculation
+AIC_func <- function(n, RSS, p){
+    return(n + 2 + n * log((2 * pi) / n) + n * log(RSS) + 2 * p)
+}
+BIC_func <- function(n, RSS, p){
+    return(n + 2 + n * log((2 * pi) / n) + n * log(RSS) + p * log(n))
+}
+AIC_small <- function(aic, n, RSS){
+    return(n + 2 + n * log((2 * pi) / n) + n * log(RSS))
+}
+
+# Initialization and pre-allocation
+count <- 0
+plot_list <- vector("list")
+AIC_list <- vector("list")
+BIC_list <- vector("list")
+
+###############################################################################
+# Model fitting
+###############################################################################
+
+for (i in unique(data_subset$ID)){
+    # Initialise
+    sub <- subset(data_subset, data_subset$ID == i)
+    N_0_start <- min(sub$log_pop)
+    N_max_start <- max(sub$log_pop)
+    n_row <- nrow(sub)
+    t <- as.numeric(sub$Time)
+    count <- count + 1
+
+    # calculation of maximum growth rate
+    r_max_start <- 0; 
+    for (x in 2:length(sub$Time)){
+        roll <- (sub[x, "log_pop"] - sub[x-1, "log_pop"])/(sub[x, "Time"] - sub[x-1, "Time"])
+        if (roll > r_max_start & roll != "NaN"){
+            r_max_start <- roll;
+            loc <- x}
+    }
+    r_max_start <- as.numeric(r_max_start)
+    t_lag_start <- sub[loc, "Time"] - (sub[loc, "log_pop"] - N_0_start) / r_max_start
+
+    # quadratic model
+    quadratic_model <- lm(PopBio ~ poly(Time, 2), data = sub)
+    quadratic_predict <- predict.lm(quadratic_model, data.frame(Time = t))
+
+    # cubic model
+    cubic_model <- lm(PopBio ~ poly(Time, 3), data = sub)
+    cubic_predict <- predict.lm(cubic_model, data.frame(Time = t))
+    
+    # buchanan model
+    fit_buchanan <- try(nlsLM(log(PopBio) ~ buchanan_model(t = Time, t_lag, t_max, N_0, r_max),
+                    data = subset,
+                    start = list(
+                    t_lag = t_lag_start,
+                    t_max = t_max_start,
+                    N_0 = N_0_start,
+                    r_max = r_max_start),
+                    control = nls.lm.control(200)),
+                    silent = TRUE)
+
+    # logistic model
+    fit_logistic <- try(nlsLM(log(PopBio) ~ logistic_model(t = Time, r_max, N_max, N_0),
+        data = sub, 
+        start = list(r_max = r_max_start, N_0 = N_0_start, N_max = N_max_start),
+        control = list(maxiter = 200)), silent = TRUE)
+
+    # gompertz model
+    fit_gompertz <- try(nlsLM(log(PopBio) ~ gompertz_model(t = Time, r_max, N_max, N_0, t_lag), 
+                        data = sub,
+                        start = list(r_max = r_max_start, N_0 = N_0_start, 
+                        N_max = N_max_start,
+                        t_lag = t_lag_start),
+                        control = nls.lm.control(maxiter = 100)),
+                        silent = TRUE)
+
+    # baranyi model
+    fit_baranyi <- try(nlsLM(log(PopBio) ~ baranyi_model(t = Time, N_0, N_max, r_max, 
+                        t_lag), 
+                        data = sub,
                         start = list(N_0 = N_0_start,
-                        t_lag = t_lag_start,
-                        t_max = t_max_start))
-summary(fit_buchanan)
+                        N_max = N_max_start,
+                        r_max = r_max_start,
+                        t_lag = t_lag_start),
+                        control = nls.lm.control(maxiter = 100)),
+                        silent = TRUE)
 
-buchanan_points <- buchanan_model(t,
-                    t_lag = coef(fit_buchanan)["t_lag"],
-                    t_max = coef(fit_buchanan)["t_max"],
-                    N_0 = coef(fit_buchanan)["N_0"],
-                    k)
+    # Model Comparison
+    # AIC and BIC generation
+    if (class(quadratic_model) == 'lm'){
+        p_quadratic <- length(coef(quadratic_model))
+        residuals_qua <- (residuals(quadratic_model))^2
+        residuals_qua[is.nan(residuals_qua)] <- 0
+        RSS_quadratic <- sum(log(residuals_qua))
+        AIC_quadratic <- AIC_func(n_row, RSS_quadratic, p_quadratic)
+        if(length(quadratic_model) < 10){
+            AIC_quadratic <- AIC_small(AIC_quadratic, n_row, RSS_quadratic)
+        }
+        BIC_quadratic <- BIC_func(n_row, RSS_quadratic, p_quadratic)
+    } else {
+        AIC_quadratic <- "NA"
+        BIC_quadratic <- "NA"
+    }
 
-df3 <- data.frame(t, buchanan_points)
-df3$model <- "Buchanan model"
-names(df3) <- c("Time", "LogN", "model")
+    if (class(cubic_model) == 'lm'){
+        p_cubic <- length(coef(cubic_model))
+        residuals_cubic <- (residuals(cubic_model))^2
+        RSS_cubic <- sum(log(residuals_cubic))
+        AIC_cubic <- AIC_func(n_row, RSS_cubic, p_cubic)
+        if(length(cubic_model) < 10){
+            AIC_cubic <- AIC_small(AIC_cubic, n_row, RSS_cubic)
+        }
+        BIC_cubic <- BIC_func(n_row, RSS_cubic, p_cubic)
+    } else {
+        AIC_cubic <- "NA"
+        BIC_cubic <- "NA"
+    }
 
-##################################
-########## Logistic model ########
-##################################
+    if (class(fit_logistic) == 'nls'){
+        p_logistic <- length(coef(fit_logistic))
+        residuals_logistic <- (residuals(fit_logistic))^2
+        RSS_logistic <- sum(residuals_logistic)
+        AIC_logistic <- AIC_func(n_row, RSS_logistic, p_logistic)
+        if(length(fit_logistic) < 10){
+            AIC_logistic <- AIC_small(AIC_logistic, n_row, RSS_logistic)
+        }
+        BIC_logistic <- BIC_func(n_row, RSS_logistic, p_logistic)
+    } else {
+        AIC_logistic <- "NA"
+        BIC_logistic <- "NA"
+    }
 
-lm_growth <- lm(log(PopBio) ~ Time, data = data_subset)
-summary(lm_growth) # to approximate the start value of growth rate
+    if (class(fit_gompertz) == 'nls'){
+        p_gompertz <- length(coef(fit_gompertz))
+        residuals_gompertz <- (residuals(fit_gompertz))^2
+        RSS_gompertz <- sum(residuals_gompertz)
+        AIC_gompertz <- AIC_func(n_row, RSS_gompertz, p_gompertz)
+        if(length(fit_gompertz) < 10){
+            AIC_gompertz <- AIC_small(AIC_gompertz, n_row, RSS_gompertz)
+        }
+        BIC_gompertz <- BIC_func(n_row, RSS_gompertz, p_gompertz)
+    } else {
+        AIC_gompertz <- "NA"
+        BIC_gompertz <- "NA"
+    }
 
-logistic_model <- function(t, r_max, K, N_0){
-    return(N_0 * K * exp(r_max * t)/(K + N_0 * (exp(r_max * t) - 1)))
-}
+    if (class(fit_baranyi) == 'nls'){
+        p_baranyi <- length(coef(fit_baranyi))
+        residuals_baranyi <- (residuals(fit_baranyi))^2
+        RSS_baranyi <- sum(residuals_baranyi)
+        AIC_baranyi <- AIC_func(n_row, RSS_baranyi, p_baranyi)
+        if(length(fit_baranyi) < 10){
+            AIC_baranyi <- AIC_small(AIC_baranyi, n_row, RSS_baranyi)
+        }
+        BIC_baranyi <- BIC_func(n_row, RSS_baranyi, p_baranyi)
+    } else {
+        AIC_baranyi <- "NA"
+        BIC_baranyi <- "NA"
+    }
 
-# defining starting parameters
-N_0_start <- min(log_pop)
-K_start <- max(log_pop)
-r_max_start <- 0.004
+    if (class(fit_buchanan) == 'nls'){
+        p_buchanan <- length(fit_buchanan)
+        residuals_buchanan <- (residuals(fit_buchanan))^2
+        RSS_buchanan <- sum(residuals_buchanan)
+        AIC_buchanan <- AIC_func(n_row, RSS_buchanan, p_buchanan)
+        if(length(fit_buchanan) < 10){
+            AIC_buchanan <- AIC_small(AIC_buchanan, n_row, RSS_buchanan)
+        }
+        BIC_buchanan <- BIC_func(n_row, RSS_buchanan, p_buchanan)
+    } else {
+        AIC_buchanan <- "NA"
+        BIC_buchanan <- "NA"
+    }    
 
-fit_logistic <- nlsLM(log(PopBio) ~ logistic_model(t = Time, r_max, K, N_0),
-    data_subset, list(r_max = r_max_start, N_0 = N_0_start, K = K_start))
-summary(fit_logistic)
+    # create dataframe
+    df1 <- data.frame(t, log(quadratic_predict))
+    df1$model <- "Quadratic"
+    names(df1) <- c("Time", "pop", "model")
 
-logistic_points <- logistic_model(t, 
+    df2 <- data.frame(t, log(cubic_predict))
+    df2$model <- "Cubic"
+    names(df2) <- c("Time", "pop", "model")
+
+    if (AIC_logistic != "NA"){
+        logistic_points <- logistic_model(t, 
                     r_max = coef(fit_logistic)["r_max"],
-                    K = coef(fit_logistic)["K"],
+                    N_max = coef(fit_logistic)["N_max"],
                     N_0 = coef(fit_logistic)["N_0"])
+        df3 <- data.frame(t, logistic_points)
+        df3$model <- "Logistic"
+        names(df3) <- c("Time", "pop", "model")
+    }
 
-df4 <- data.frame(t, logistic_points)
-df4$model <- "Logistic equation"
-names(df4) <- c("Time", "LogN", "model")
-
-##########################
-##### Gompertz model ##### (Zwietering)
-##########################
-
-# functon for Gompertz model
-gompertz_model <- function(t, r_max, K, N_0, t_lag){
-    return(N_0 + (K - N_0) * exp(-exp(r_max * exp(1) * (t_lag - t)/((K - N_0) * log(10)) + 1)))
-}
-
-# starting value
-N_0_start <- min(log_pop)
-K_start <- max(log_pop)
-r_max_start <- 0.004
-t_lag_start <- data_subset$Time[which.max(diff(diff(data_subset$log_pop)))]
-
-# fitting the model
-fit_gompertz <- nlsLM(log_pop ~ gompertz_model(t = Time, r_max, K, N_0, t_lag), data_subset,
-                    list(t_lag = t_lag_start, r_max = r_max_start, N_0 = N_0_start, K = K_start))
-
-summary(fit_gompertz)
-
-gompertz_points <- gompertz_model(t,
+    if (AIC_gompertz != "NA"){
+        gompertz_points <- gompertz_model(t, 
                     r_max = coef(fit_gompertz)["r_max"],
-                    K = coef(fit_gompertz)["K"],
+                    N_max = coef(fit_gompertz)["N_max"],
                     N_0 = coef(fit_gompertz)["N_0"],
                     t_lag = coef(fit_gompertz)["t_lag"])
+        df4 <- data.frame(t, gompertz_points)
+        df4$model <- "Gompertz"
+        names(df4) <- c("Time", "pop", "model")
+    }
 
-df5 <- data.frame(t, gompertz_points)
-df5$model <- "Gompertz model"
-names(df5) <- c("Time", "LogN", "model")
+    if (AIC_baranyi != "NA"){
+        baranyi_points <- baranyi_model(t, 
+                    N_max = coef(fit_baranyi)["N_max"],
+                    N_0 = coef(fit_baranyi)["N_0"],
+                    r_max = coef(fit_baranyi)["r_max"],
+                    t_lag = coef(fit_baranyi)["t_lag"])
+        df5 <- data.frame(t, baranyi_points)
+        df5$model <- "Baranyi"
+        names(df5) <- c("Time", "pop", "model")
+    } else {
+        NA_5 <- rep(NA, length(t))
+        df5 <- data.frame(t, NA_5)
+        df5$model <- "Baranyi"
+        names(df5) <- c("Time", "pop", "model")
+    }
 
-###############################
-####### Baranyi model #########
-###############################
+    if (AIC_buchanan != "NA"){
+        buchanan_points <- buchanan_model(t,
+                    t_lag = coef(fit_buchanan)["t_lag"],
+                    t_max = coef(fit_buchanan)["t_max"],
+                    N_max = coef(fit_buchanan)["N_max"],
+                    N_0 = coef(fit_buchanan)["N_0"],
+                    r_max = coef(fit_buchanan)["r_max"])
+        df6 <- data.frame(t, buchanan_points)
+        df6$model <- "Buchanan"
+        names(df6) <- c("Time", "pop", "model")
+    } else {
+        NA_6 <- rep(NA, length(t))
+        df6 <- data.frame(t, NA_6)
+        df6$model <- "Buchanan"
+        names(df6) <- c("Time", "pop", "model")
+    }
 
-# function for Baranyi model
+    AIC_list[[i]] <- c(count + 1, AIC_quadratic, AIC_cubic, AIC_logistic, AIC_gompertz, 
+                    AIC_baranyi, AIC_buchanan)
+    BIC_list[[i]] <- c(count + 1, BIC_quadratic, BIC_cubic, BIC_logistic, BIC_gompertz,
+                    BIC_baranyi, BIC_buchanan)
 
-baranyi_model <- function(t, y_0, y_max, r_max, h_0, v){
-    return(y_0 + r_max * t + (1/r_max) * (log(exp(-v * t) + exp(-h_0) - exp(-v * t - h_0))) - log(1 + (exp(r_max * t) + (1/r_max) * log(exp(-v*t)+exp(-h_0)+exp(-v*t - h_0)) - 1) / exp(y_max - y_0)))
+    # Creating plots
+    results <- rbind(df1, df2, df3, df4, df5, df6)
+    p <- ggplot(data = sub, aes(x = Time, y = log_pop)) +
+        geom_point(size = 3) +
+        geom_line(data = results, aes(x = Time, y = pop, col = model), size = 1) +
+        theme_bw() +
+        theme(aspect.ratio = 1) +
+        labs(x = "Time", y = "log(Abundance)")
+    plot_list[[i]] <- p
 }
 
-# y0 = ln(x0), ymax = ln(xmax), x0 is the initial and xmax the asymptotic cell concentration
-# yt = ln(xt), xt the cell concentration, PopBio
-# mumax, maximum secific growth rate
-# m, curvature parameter to characterize the transition from the exponential phase
-# h0, dimensionless parameter quantifying the initial physiological state of the cells
+AIC_data <- data.frame(do.call(cbind, AIC_list))
+AIC_data <- data.frame(AIC = c("", "quadratic", "cubic", "logistic",
+                    "gompertz", "baranyi", "buchanan"), AIC_data)
+BIC_data <- data.frame(do.call(cbind, BIC_list))
+BIC_data <- data.frame(BIC = c("", "quadratic", "cubic", "logistic",
+                    "gompertz", "baranyi", "buchanan"), BIC_data)
 
-# starting value
-y_0_start <- min(log_pop)
-y_max_start <- max(log_pop)
-r_max_start <- 0.004
-h_0_start <- (log_pop[28] - log_pop[29]) / (t[28] - t[29]) # the initial growth rate
 
-# fitting the model using the defined starting values
-fit_baranyi <- nlsLM(log_pop ~ baranyi_model(t = Time, y_0, y_max, r_max,
-    h_0, v = r_max), data = data_subset,
-                    start = list(y_0 = y_0_start,
-                    y_max = y_max_start,
-                    r_max = r_max_start,
-                    h_0 = h_0_start))
-summary(fit_baranyi)
-
-baranyi_points <- baranyi_model(t,
-                    y_0 = coef(fit_baranyi)["y_0"],
-                    y_max = coef(fit_baranyi)["y_max"],
-                    r_max = coef(fit_baranyi)["r_max"],
-                    h_0 = coef(fit_baranyi)["h_0"],
-                    v = coef(fit_baranyi)["r_max"])
-
-df6 <- data.frame(t, baranyi_points)
-df6$model <- "Baranyi model"
-names(df6) <- c("Time", "LogN", "model")
+pdf("../results/plot_subsets.pdf")
+print(plot_list)
+graphics.off()
 
 ##################################
 ###### Comparing the models ######
 ##################################
 
-model_frame <- rbind(df1, df2, df3, df4, df5, df6)
+#model_frame <- rbind(df1, df2, df3, df4, df5, df6)
 
 # generation of the graph
-p <- ggplot(data_subset, aes(x = t, y = log_pop)) +
-    geom_point(size = 3) +
-    geom_line(data = model_frame, aes(x = Time, y = LogN, col = model),
-        linewidth = 1) +
-    theme(aspect.ratio = 1) +
-    labs(x = "Time", y = "Cell number")
+#p <- ggplot(data_subset, aes(x = t, y = log_pop)) +
+#    geom_point(size = 3) +
+#    geom_line(data = model_frame, aes(x = Time, y = LogN, col = model),
+#        linewidth = 1) +
+#    theme(aspect.ratio = 1) +
+#    labs(x = "Time", y = "Cell number")
 
 # AIC and BIC values of the fitted models
-AIC <- data.frame("AIC", AIC(quadratic_model), AIC(cubic_model),
-    AIC(fit_buchanan), AIC(fit_baranyi), AIC(fit_logistic), AIC(fit_gompertz))
-BIC <- data.frame("BIC", BIC(quadratic_model), BIC(cubic_model),
-    BIC(fit_buchanan), BIC(fit_baranyi), BIC(fit_logistic), BIC(fit_gompertz))
-names(AIC) <- c("Criteria", "Quadratic", "Cubic", "Buchanan",
-    "Baranyi", "Logistic", "Gompertz")
-names(BIC) <- c("Criteria", "Quadratic", "Cubic", "Buchanan", 
-    "Baranyi", "Logistic", "Gompertz")
-comparison <- rbind(AIC, BIC)
-write.csv(comparison, "../results/model_comparison.csv",
-    row.names = TRUE)
+#AIC <- data.frame("AIC", AIC(quadratic_model), AIC(cubic_model),
+#    AIC(fit_buchanan), AIC(fit_baranyi), AIC(fit_logistic), AIC(fit_gompertz))
+#BIC <- data.frame("BIC", BIC(quadratic_model), BIC(cubic_model),
+#    BIC(fit_buchanan), BIC(fit_baranyi), BIC(fit_logistic), BIC(fit_gompertz))
+#names(AIC) <- c("Criteria", "Quadratic", "Cubic", "Buchanan",
+#    "Baranyi", "Logistic", "Gompertz")
+#names(BIC) <- c("Criteria", "Quadratic", "Cubic", "Buchanan", 
+#    "Baranyi", "Logistic", "Gompertz")
+#comparison <- rbind(AIC, BIC)
+#write.csv(comparison, "../results/model_comparison.csv",
+#    row.names = TRUE)
 
+##
